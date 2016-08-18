8ccfe1a779690c3c5ec40064d3c5d1e7c641b0db
Michael Petersen <mpetason@gmail.com>
2016-08-18 16:28:19 -0700

    updated

--- keystone/find_and_restart_services.sh
+++ keystone/find_and_restart_services.sh
@@ -31,15 +31,15 @@ compFilenames=(
 for c in $contNum; do
 	printf "Checking on Controller: ${c}\n"
 	for f in ${contFilenames[*]}; do
-		serviceNamesCont+=$(ssh -n -q root@node-$c "ps aux | grep -i " $f| grep -i '/usr/bin' | awk '{print $12}')
-	done | sort -u
+		serviceNamesCont+=$(ssh -n -q root@node-$c "ps aux | grep -i " $f| grep -i '/usr/bin' | awk '{print $12}' |sort -u)
+	done
 done	
 
 for c in $compNum; do
 	printf "Checking on Computes: ${c}\n"
 	for f in ${contFileNames[*]}; do 
-		serviceNamesComp+=$(ssh -n -q root@node-$c "ps aux | grep -i " $f| grep -i '/usr/bin' | awk '{print $12}')
-	done | sort -u
+		serviceNamesComp+=$(ssh -n -q root@node-$c "ps aux | grep -i " $f| grep -i '/usr/bin' | awk '{print $12}'| sort -u)
+	done
 done
 
 for s in $serviceNamesComp[*]; do
