b0c7d22f863e77d1458825ea824102ecc2c97136
Michael Petersen <mpetason@gmail.com>
2016-08-18 12:43:15 -0700

    Reverted Admin

--- keystone/update_admin_endpoint_to_ssl.sh
+++ keystone/update_admin_endpoint_to_ssl.sh
@@ -13,18 +13,7 @@ oldAdmin=http://10.109.1.2:35357
 ## to keep :35357 as it is the admin port.
 newAdmin=http://public.fuel.local:35357
 
-## We have the option to use different fields based on needs.
-## By default we'll use field 1, which is the ID of the node.
-## We can use fiel 10, which is the IP of the node, if the
-## node isn't reachable by using `ssh node-#`
-fieldNumber=1
-
-## Here we pull the values based on the field. If the field 
-## is equal to 1 then we'll have a list of IDs, if it is 
-## field 10 we'll receive a list of IPs.
-contNum=$(fuel node |grep cont | awk '{print $'$fieldNumber'}')
-
-compNum=$(fuel node|grep comp | awk '{print $'$fieldNumber'}')
+contNum=$(fuel node|grep cont | awk '{print $1}')
 
 ## List of filenames we need to edit on a Controller node
 ## after making the updates to Keystone.
@@ -40,9 +29,6 @@ for c in $contNum; do
 	echo $c
 done
 
-for c in $compNum; do
-	echo $c
-done
 #for c in $conNum; do
 #    ssh -n node-$c sed -i -e 's/$oldAdmin/$newAdmin/g' $filename
 #done
